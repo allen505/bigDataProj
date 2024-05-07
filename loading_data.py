59fce3155274da53f63837a29f845f3236dacf4d
Kuber Mehra <kubermehra2000@gmail.com>
2024-05-07 15:57:46 -0400

    "changes to py script"

--- loading_data.py
+++ loading_data.py
@@ -5,6 +5,8 @@
 # ### Unzip
 
 # %%
+
+print("Unzipping file")
 import py7zr
 
 with py7zr.SevenZipFile('Dataset/US_youtube_trending_data.7z', mode='r') as z:
@@ -16,6 +18,8 @@ print("Extracted data")
 # #### Importing dependencies
 
 # %%
+print("***********************************************")
+print("\n Loading Dependenceis")
 import os
 import pprint
 import pandas as pd
@@ -32,23 +36,28 @@ import pprint
 # collection name: videos
 
 # %%
+print("***********************************************")
+print("\n Connecting to db")
 client = MongoClient(host='localhost',port=27017)
-
-print("Connected to MongoDB")
+print(f"Connection established :{client.HOST}")
 # %%
-client.list_database_names()
+print(client.list_database_names())
 
 # %% [markdown]
 # #### Creating database
 
 # %%
+print("***********************************************")
+print("\n creating database")
 db=client["bigdata"]
-
+print("database created")
 # %%
+print("creating collections")
 db.create_collection("videos")
+print("collection created")
 
 # %%
-db.list_collection_names()
+print(db.list_collection_names())
 
 # %%
 videos=db["videos"]
@@ -57,20 +66,23 @@ videos=db["videos"]
 # ### Inseting data
 
 # %%
+print("Loading csv")
 videos_data_dict=pd.read_csv("US_youtube_trending_data.csv").to_dict(orient="records")
-len(videos_data_dict)
+print(len(videos_data_dict))
 
 # %%
 os.remove("US_youtube_trending_data.csv")
 
 # %%
+
+print("add data")
 videos.insert_many(videos_data_dict)
 
 # %%
-videos.count_documents({})
+print(videos.count_documents({}))
 
 # %%
 for i in videos.find({}).limit(3):
     print(i)
-
+print("!Done")
 
