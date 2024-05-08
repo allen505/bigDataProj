14d99d6b9419c789f48f90bff7ca27f53311c91d
Kuber Mehra <kubermehra2000@gmail.com>
2024-05-08 14:42:32 -0400

    Changes in kafka to transfer json over network

--- kafka_demo/kafka_consumer.py
+++ kafka_demo/kafka_consumer.py
@@ -8,20 +8,22 @@ PORT = '9093'
 consumer = KafkaConsumer(
     TOPIC_NAME,
     # bootstrap_servers=['localhost:9092'],
-    bootstrap_servers=['localhost:' + PORT],
+    bootstrap_servers=['localhost:9093'],
     group_id=CONSUMER_GRP,
-    # value_deserializer=lambda x: loads(x.decode('utf-8'))
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
     )
 
 print('Connected to Kafka')
 
-for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+# while True:
+#     for message in consumer:
+#         # message value and key are raw bytes -- decode if necessary!
+#         # e.g., for unicode: `message.value.decode('utf-8')`
+#         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                             message.offset, message.key,
+#                                             message.value))
 
-# for message in consumer:
-#     message = message.value
-#     print('Received value: {}'.format(message))
+while True:
+    for message in consumer:
+        message = message.value
+        print('Received value: {}'.format(message))
--- kafka_demo/kafka_producer.py
+++ kafka_demo/kafka_producer.py
@@ -13,26 +13,25 @@ PORT = '9093'
 
 producer = KafkaProducer(
     # bootstrap_servers=['localhost:9092'],
-    bootstrap_servers=['localhost' + PORT],
-    # value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    bootstrap_servers=['localhost:9093'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
 
 print('Connected to Kafka')
 
-future = producer.send(TOPIC_NAME, b'testVal', b'testKey')
+# future = producer.send(TOPIC_NAME, b'testVal', b'testKey')
 
-print('Sent the value')
+# print('Sent the value')
 
-record_metadata = None
-# Block for 'synchronous' sends
-try:
-    record_metadata = future.get(timeout=10)
-except KafkaError as e:
-    # Decide what to do if produce request failed...
-    print('Kafka error: ', e)
+# record_metadata = None
+# # Block for 'synchronous' sends
+# try:
+#     record_metadata = future.get(timeout=10)
+# except KafkaError as e:
+#     # Decide what to do if produce request failed...
+#     print('Kafka error: ', e)
 
-#
-# for e in range(10):
-#     data = {'number': e}
-#     print('Sending {} to Kafka'.format(e))
-#     producer.send(TOPIC_NAME, value=data)
+for e in range(10):
+    data = {'number': e}
+    print('Sending {} to Kafka'.format(e))
+    producer.send(TOPIC_NAME, value=data)
