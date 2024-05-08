126307e2e50dc28c497f1570b0e37ab4d34f82c9
Allen <allenabraham505@gmail.com>
2024-05-08 14:25:08 -0400

    Pushing changes which were not working on my local

--- kafka_demo/kafka_consumer.py
+++ kafka_demo/kafka_consumer.py
@@ -1,21 +1,27 @@
 from json import loads
 from kafka import KafkaConsumer
 
-TOPIC_NAME = 'vidRecommender'
+TOPIC_NAME = 'test-topic'
 CONSUMER_GRP = 'my-group'
+PORT = '9093'
 
 consumer = KafkaConsumer(
     TOPIC_NAME,
-    bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
+    # bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['localhost:' + PORT],
     group_id=CONSUMER_GRP,
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
-
-TOPIC_NAME = 'vidRecommender'
+    # value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
 
 print('Connected to Kafka')
 
 for message in consumer:
-    message = message.value
-    print('Received value: {}'.format(message))
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+
+# for message in consumer:
+#     message = message.value
+#     print('Received value: {}'.format(message))
--- kafka_demo/kafka_producer.py
+++ kafka_demo/kafka_producer.py
@@ -1,19 +1,38 @@
 from time import sleep
-from json import dumps
+import json
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import logging
 
-TOPIC_NAME = 'vidRecommender'
+logger = logging.getLogger('kafka')
+logger.setLevel(logging.DEBUG)
+
+TOPIC_NAME = 'test-topic'
 sleepTime = 1
+PORT = '9093'
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
+producer = KafkaProducer(
+    # bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['localhost' + PORT],
+    # value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
 
 print('Connected to Kafka')
 
-for e in range(10):
-    data = {'number': e}
-    print('Sending {} to Kafka'.format(e))
-    producer.send(TOPIC_NAME, value=data)
-    print('Sleeping for {} seconds'.format(sleepTime))
-    sleep(sleepTime)
+future = producer.send(TOPIC_NAME, b'testVal', b'testKey')
+
+print('Sent the value')
+
+record_metadata = None
+# Block for 'synchronous' sends
+try:
+    record_metadata = future.get(timeout=10)
+except KafkaError as e:
+    # Decide what to do if produce request failed...
+    print('Kafka error: ', e)
+
+#
+# for e in range(10):
+#     data = {'number': e}
+#     print('Sending {} to Kafka'.format(e))
+#     producer.send(TOPIC_NAME, value=data)
