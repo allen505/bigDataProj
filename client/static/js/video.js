4df8c5bd4b9540e6a44ff0648db6f4920d0c567f
Kuber Mehra <kubermehra2000@gmail.com>
2024-05-09 02:05:34 -0400

    Working video iframe and all changes made video.js and play_video.html

--- client/static/js/video.js
+++ client/static/js/video.js
@@ -2,11 +2,13 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var video=document.getElementsByTagName('h1')[0];
+id=video.id
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('video-frame', {
     height: '200',
     width: '400',
-    videoId: '0a5YhsnITFE',
+    videoId: id,
     playerVars: {
         'autoplay': 0, // This is optional, 1 for autoplay when the video loads
         'controls': 1, // This is optional, 0 to hide player controls
